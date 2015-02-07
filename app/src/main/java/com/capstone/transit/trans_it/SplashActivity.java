98c3ef9fad3b1c66f4176725dc2c3d4882ffc9d8
Thomas Miele <thomas.miele@hotmail.com>
2015-02-06 19:21:32 -0500

    Created initial Splash and Main Menu

---
+++ app/src/main/java/com/capstone/transit/trans_it/SplashActivity.java
@@ -0,0 +1,62 @@
+package com.capstone.transit.trans_it;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * Created by Thomas on 2/6/2015.
+ */
+
+public class SplashActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+
+        Thread logoTimer = new Thread(){
+            public void run(){
+                try{
+                    sleep(1000);
+                    startActivity(new Intent("com.capstone.transit.trans_it.MAINMENU"));
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                finally{
+                    finish();
+                }
+            }
+        };
+        logoTimer.start();
+
+    }
+
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+
+}
