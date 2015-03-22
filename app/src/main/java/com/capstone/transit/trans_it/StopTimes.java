62fcc44580d6a3710578ac037c8c004a1eca8334
matt662 <matthewf662@gmail.com>
2015-03-21 23:36:28 -0400

    created setters and getters for stoptimes

--- app/src/main/java/com/capstone/transit/trans_it/StopTimes.java
+++ app/src/main/java/com/capstone/transit/trans_it/StopTimes.java
@@ -6,34 +6,97 @@ import android.graphics.Color;
  * Created by matt on 18/03/15.
  */
 public class StopTimes {
-    private String time, route, trip;
+    private String time, route, trip, vehicleNumber, toDisplay;
     private int timeColor = Color.BLACK;
     private int delay = 0;
     private boolean isRealtime;
 
     //Constructor
-    public StopTimes (String inTime, String inRoute, String inTrip, boolean realtime, int inDelay){
+    public StopTimes (String inTime, String inRoute, String inTrip, boolean realtime, int inDelay,String inVehicleNumber){
         time = inTime;
         route = inRoute;
+
         trip = inTrip;
         isRealtime = realtime;
         delay = inDelay;
-        if (realtime == true){
-            if (delay > 180){
+        vehicleNumber = inVehicleNumber;
+        if (isRealtime){
+            if (delay > 180) {
                 timeColor = Color.RED;
             }
             else timeColor = Color.GREEN;
         }
         else timeColor = Color.BLACK;
+        toDisplay = time + " " + vehicleNumber;
     }
 
     public void setTime (String inTime){
-        time = inTime;
+        this.time = inTime;
     }
-    public void setRouteName (String name){
-        route = name;
+
+    public String getTime() {
+        return time;
     }
-    public int getColor (){
+
+    public String getRoute() {
+        return route;
+    }
+
+    public void setRoute(String route) {
+        this.route = route;
+    }
+
+    public String getTrip() {
+        return trip;
+    }
+
+    public void setTrip(String trip) {
+        this.trip = trip;
+    }
+
+    public String getVehicleNumber() {
+        return vehicleNumber;
+    }
+
+    public void setVehicleNumber(String vehicleNumber) {
+        this.vehicleNumber = vehicleNumber;
+    }
+
+    public String getToDisplay() {
+        return toDisplay;
+    }
+
+    public void setToDisplay(String toDisplay) {
+        this.toDisplay = toDisplay;
+    }
+
+    public int getTimeColor() {
         return timeColor;
     }
+
+    public void setTimeColor(int timeColor) {
+        this.timeColor = timeColor;
+    }
+
+    public int getDelay() {
+        return delay;
+    }
+
+    public void setDelay(int delay) {
+        this.delay = delay;
+        if (isRealtime){
+            if (this.delay > 180) timeColor = Color.RED;
+            else timeColor = Color.GREEN;
+        }
+    }
+
+    public boolean isRealtime() {
+        return isRealtime;
+    }
+
+    public void setRealtime(boolean isRealtime) {
+        this.isRealtime = isRealtime;
+    }
+
+
 }
