c5045e296ffae4e6a2ba91c958b05bcc6c03bc28
Thomas Miele <thomas.miele@hotmail.com>
2015-03-30 21:35:31 -0400

    Got auto complete working in trip planner!

---
+++ app/src/main/java/com/capstone/transit/trans_it/TripPlannerActivity.java
@@ -0,0 +1,46 @@
+package com.capstone.transit.trans_it;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AutoCompleteTextView;
+
+
+public class TripPlannerActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trip_planner);
+
+        AutoCompleteTextView Start = (AutoCompleteTextView) findViewById(R.id.Start);
+        AutoCompleteTextView End = (AutoCompleteTextView) findViewById(R.id.End);
+
+        Start.setAdapter(new PlacesAutoCompleteAdapter(this, R.layout.trip_list_item));
+        End.setAdapter(new PlacesAutoCompleteAdapter(this, R.layout.trip_list_item));
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_trip_planner, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
