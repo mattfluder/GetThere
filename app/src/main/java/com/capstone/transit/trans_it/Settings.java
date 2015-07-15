93c08a18dbbc906029c6e3d5cf6d3b7b5e213ae1
matt662 <matthewf662@gmail.com>
2015-07-14 20:04:33 -0400

    Added license and license preamble to source files. GNU General Public License v3

--- app/src/main/java/com/capstone/transit/trans_it/Settings.java
+++ app/src/main/java/com/capstone/transit/trans_it/Settings.java
@@ -1,3 +1,27 @@
+/**
+ *
+ This file is part of the "Get There!" application for android developed
+ for the SFWR ENG 4G06 Capstone course in the 2014/2015 Fall/Winter
+ terms at McMaster University.
+
+
+ Copyright (C) 2015 M. Fluder, T. Miele, N. Mio, M. Ngo, and J. Rabaya
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
+
 package com.capstone.transit.trans_it;
 
 import android.app.Notification;
